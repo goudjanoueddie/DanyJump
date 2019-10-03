5c40d6f4268bf25890b1d83159c89c330f89f9f5
goudjanou <goudjanoueddie@gmail.com>
2019-10-03 16:49:47 -0400

    level III Ended

---
+++ app/src/main/java/org/jdeveloper/danyjump/TomatoRedEnemy.java
@@ -0,0 +1,74 @@
+package org.jdeveloper.danyjump;
+
+
+import android.content.Context;
+
+public class TomatoRedEnemy extends GameObject {
+
+    private float waypointX1;
+    private float waypointX2;
+    private int currentWaypoint;
+    final float MAX_X_VELOCITY=10;
+
+
+    TomatoRedEnemy(Context context,float worldStartX,float worldStartY,char type,int pixelsPerMetre) {
+
+        final int ANIMATION_FPS=14;
+        final int ANIMATION_FRAME_COUNT=14;
+        final String BITMAP_NAME="tomatored";
+        final float HEIGHT=2;
+        final float WIDTH=1;
+
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+
+        setBitmapName(BITMAP_NAME);
+        setMoves(true);
+        setActive(true);
+        setVisible(true);
+
+
+        setAnimFps(ANIMATION_FPS);
+        setAnimFrameCount(ANIMATION_FRAME_COUNT);
+        setAnimated(context,pixelsPerMetre,true);
+
+        setWorldLocation(worldStartX,worldStartY,0);
+        setxVelocity(-MAX_X_VELOCITY);
+        currentWaypoint=1;
+
+    }
+
+    public void setWaypoints(float x1,float x2){
+        waypointX1=x1;
+        waypointX2=x2;
+    }
+
+    public void update(long fps,float gravity){
+
+        if(currentWaypoint ==1){
+            if(getWorldLocation().x <=waypointX1){
+
+                currentWaypoint=2;
+                setxVelocity(MAX_X_VELOCITY);
+                setFacing(RIGHT);
+            }
+
+        }
+
+        if (currentWaypoint ==2){
+
+            if (getWorldLocation().x >=waypointX2){
+
+                currentWaypoint=1;
+                setxVelocity(-MAX_X_VELOCITY);
+                setFacing(LEFT);
+            }
+
+        }
+
+        move(fps);
+        setRectHitbox();
+
+    }
+}
--- app/src/main/java/org/jdeveloper/danyjump/TomatoYellowEnemy.java
+++ app/src/main/java/org/jdeveloper/danyjump/TomatoYellowEnemy.java
@@ -1,5 +1,7 @@
 package org.jdeveloper.danyjump;
 
+import android.content.Context;
+
 public class TomatoYellowEnemy extends GameObject {
 
     private float waypointX1;
@@ -7,8 +9,60 @@ public class TomatoYellowEnemy extends GameObject {
     private int currentWaypoint;
     final float MAX_X_VELOCITY=3;
 
+    TomatoYellowEnemy(Context context,float worldStartX,float worldStartY,char type,int pixelsPerMetre){
+
+        final int ANIMATION_FPS=14;
+        final int ANIMATION_FRAME_COUNT=14;
+        final String BITMAP_NAME="tomatoyellow";
+        final float HEIGHT=2;
+        final float WIDTH=1;
+
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+
+        setBitmapName(BITMAP_NAME);
+        setMoves(true);
+        setActive(true);
+        setVisible(true);
+
+        setAnimFps(ANIMATION_FPS);
+        setAnimFrameCount(ANIMATION_FRAME_COUNT);
+        setAnimated(context,pixelsPerMetre,true);
+
+        setWorldLocation(worldStartX,worldStartY,0);
+        setxVelocity(-MAX_X_VELOCITY);
+        currentWaypoint=1;
+
+    }
+
+    public void setWaypoints(float x1,float x2){
+        waypointX1=x1;
+        waypointX2=x2;
+    }
+
 
     public void update(long fps,float gravity){
+
+        if (currentWaypoint ==1){
+            if (getWorldLocation().x <=waypointX1){
+                currentWaypoint=2;
+                setxVelocity(MAX_X_VELOCITY);
+                setFacing(RIGHT);
+
+            }
+        }
+
+        if (currentWaypoint ==2){
+            if (getWorldLocation().x >=waypointX2){
+                currentWaypoint=1;
+                setxVelocity(-MAX_X_VELOCITY);
+                setFacing(LEFT);
+            }
+        }
+
+        move(fps);
+        setRectHitbox();
 
     }
 }

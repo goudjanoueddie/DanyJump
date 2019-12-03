02259cad63475b41f10fbde7bcd11fb3d18c1f23
goudjanou <goudjanoueddie@gmail.com>
2019-12-03 13:32:45 -0500

    level VI in progress.Middle of the level. Added Vertical Enemy like an fire

---
+++ app/src/main/java/org/jdeveloper/danyjump/FireSpriteBlueEnemy.java
@@ -0,0 +1,76 @@
+package org.jdeveloper.danyjump;
+
+import android.content.Context;
+
+public class FireSpriteBlueEnemy extends GameObject {
+
+    private float waypointhight;
+    private float waypointbottom;
+    private int currentWaypoint;
+    final float MAX_Y_VELOCITY=1;
+
+
+    FireSpriteBlueEnemy(Context context, float worldStartX, float worldStartY, char type, int pixelsPerMetre){
+
+        final int ANIMATION_FPS=8;
+        final int ANIMATION_FRAME_COUNT=1;
+        final String BITMAP_NAME="firespriteblue";
+
+
+        final float HEIGHT=4;
+        final float WIDTH=1;
+
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+
+        setBitmapName(BITMAP_NAME);
+        setMoves(true);
+        setActive(true);
+        setVisible(true);
+
+        //Moving the bitmap
+        setAnimFps(ANIMATION_FPS);
+        setAnimFrameCount(ANIMATION_FRAME_COUNT);
+        setAnimated(context,pixelsPerMetre,true);
+
+        setWorldLocation(worldStartX,worldStartY,0);
+        setyVelocity(-MAX_Y_VELOCITY);
+        currentWaypoint=1;
+
+    }
+
+    public void setWaypoints(float x1,float x2){
+
+        waypointhight=x1;
+        waypointbottom=x2;
+
+    }
+
+
+    public void update(long fps,float gravity){
+
+        if (currentWaypoint ==1){
+
+            if (getWorldLocation().y <= waypointhight){
+                currentWaypoint=2;
+                setyVelocity(MAX_Y_VELOCITY);
+            }
+
+        }
+
+        if (currentWaypoint ==2){
+
+            if (getWorldLocation().y >= waypointbottom){
+                currentWaypoint =1;
+                setyVelocity(-MAX_Y_VELOCITY);
+            }
+
+        }
+
+        move(fps);
+        setRectHitbox();
+
+    }
+
+}
---
+++ app/src/main/java/org/jdeveloper/danyjump/FireSpriteRedEnemy.java
@@ -0,0 +1,73 @@
+package org.jdeveloper.danyjump;
+
+import android.content.Context;
+
+public class FireSpriteRedEnemy extends GameObject {
+
+    private float waypointhight;
+    private float waypointbottom;
+    private int currentWaypoint;
+    final float MAX_Y_VELOCITY=6;
+
+
+    FireSpriteRedEnemy(Context context, float worldStartX, float worldStartY, char type, int pixelsPerMetre){
+
+        final int ANIMATION_FPS=8;
+        final int ANIMATION_FRAME_COUNT=1;
+        final String BITMAP_NAME="firespritered";
+
+        final float HEIGHT=4;
+        final float WIDTH=1;
+
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+
+        setBitmapName(BITMAP_NAME);
+        setMoves(true);
+        setActive(true);
+        setVisible(true);
+
+        //MOving the bitmap
+        setAnimFps(ANIMATION_FPS);
+        setAnimFrameCount(ANIMATION_FRAME_COUNT);
+        setAnimated(context,pixelsPerMetre,true);
+
+        setWorldLocation(worldStartX,worldStartY,0);
+        setyVelocity(-MAX_Y_VELOCITY);
+        currentWaypoint=1;
+
+    }
+
+    public void setWaypoints(float x1,float x2){
+
+        waypointhight=x1;
+        waypointbottom=x2;
+
+
+    }
+
+
+    public void update(long fps,float gravity){
+
+        if (currentWaypoint ==1){
+            if (getWorldLocation().y <= waypointhight){
+                currentWaypoint=2;
+                setyVelocity(MAX_Y_VELOCITY);
+            }
+        }
+
+        if (currentWaypoint ==2){
+
+            if (getWorldLocation().y >= waypointbottom){
+                currentWaypoint =1;
+                setyVelocity(-MAX_Y_VELOCITY);
+            }
+        }
+
+        move(fps);
+        setRectHitbox();
+
+
+    }
+}

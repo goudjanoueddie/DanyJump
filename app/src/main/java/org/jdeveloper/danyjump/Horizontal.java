5e8366079a5db0b4db3ec6b328d79ba906acf272
goudjanou <goudjanoueddie@gmail.com>
2019-12-24 17:28:49 -0500

    level VII in progress added HorizontBarre,HoritontalbarreBlack and several other things

---
+++ app/src/main/java/org/jdeveloper/danyjump/Horizontal.java
@@ -0,0 +1,69 @@
+package org.jdeveloper.danyjump;
+
+import android.content.Context;
+
+public class Horizontal extends GameObject {
+
+
+    private float waypointX1;
+    private float waypointX2;
+    private int currentWaypoint;
+    final float MAX_X_VELOCITY=3;
+
+    Horizontal(Context context,float worldStartX,float worldStartY,char type,int pixelsPerMetre) {
+
+        setTraversable();
+        final float HEIGHT=1;
+        final float WIDTH=3;
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+        setBitmapName("horizontalbarre");
+        setWorldLocation(worldStartX,worldStartY,0);
+        setRectHitbox();
+
+        setMoves(true);
+        setActive(true);
+        setVisible(true);
+
+        //setAnimFps(ANIMATION_FPS);
+        //setAnimFrameCount(ANIMATION_FRAME_COUNT);
+        setAnimated(context,pixelsPerMetre,true);
+
+        setWorldLocation(worldStartX,worldStartY,0);
+        setxVelocity(-MAX_X_VELOCITY);
+        currentWaypoint=1;
+
+    }
+
+    public void setWaypoints(float x1,float x2){
+
+        waypointX1=x1;
+        waypointX2=x2;
+
+    }
+
+    public void update(long fps,float gravity){
+
+        if (currentWaypoint ==1){//Heading left
+            if (getWorldLocation().x <= waypointX1){
+                currentWaypoint=2;
+                setxVelocity(MAX_X_VELOCITY);
+                setFacing(RIGHT);
+            }
+
+        }
+
+        if (currentWaypoint ==2){//Heading right
+            if (getWorldLocation().x >= waypointX2){
+                currentWaypoint =1;
+                setxVelocity(-MAX_X_VELOCITY);
+                setFacing(LEFT);
+            }
+
+        }
+        move(fps);
+        setRectHitbox();
+
+    }
+}

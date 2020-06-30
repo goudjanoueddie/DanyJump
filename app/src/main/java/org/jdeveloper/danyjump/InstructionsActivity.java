1803c7feb45a7afd47d0a2780e743e7ca0f4a962
goudjanou <goudjanoueddie@gmail.com>
2020-06-30 18:11:17 -0400

    start screen instructions finished

---
+++ app/src/main/java/org/jdeveloper/danyjump/InstructionsActivity.java
@@ -0,0 +1,24 @@
+package org.jdeveloper.danyjump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class InstructionsActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+
+
+    @Override
+    public void onClick(View v){
+
+    }
+
+
+}
--- app/src/main/java/org/jdeveloper/danyjump/MenuActivity.java
+++ app/src/main/java/org/jdeveloper/danyjump/MenuActivity.java
@@ -1,14 +1,54 @@
 package org.jdeveloper.danyjump;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends Activity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+         imageView=(ImageView)findViewById(R.id.imageCube);
+        //imageView.setRotation(imageView.getRotation() +90);
+
+        final Button buttonInstructions=(Button)findViewById(R.id.btnInstructions);
+        //buttonInstructions.setOnClickListener(btinstructions());
+
+    }
+
+    /*@Override
+    public void run() {
+
+        try {
+            while (true) {
+                imageView.setRotation(imageView.getRotation() +90);
+            }
+        } finally {
+            //System.out.println("Fin demon");
+        }
+
+
+
+    }*/
+
+    public void btnClick(View v){
+
+    }
+
+    public void btinstructions(View v){
+        Intent i=new Intent(this,InstructionsActivity.class);
+        startActivity(i);
+        finish();
+
     }
 }

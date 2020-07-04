37a39cd6be4656f960fd35a99eb3f2716722427f
goudjanou <goudjanoueddie@gmail.com>
2020-07-04 15:32:50 -0400

    Instructions Screen Finished

--- app/src/main/java/org/jdeveloper/danyjump/InstructionsActivity.java
+++ app/src/main/java/org/jdeveloper/danyjump/InstructionsActivity.java
@@ -3,8 +3,10 @@ package org.jdeveloper.danyjump;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class InstructionsActivity extends Activity implements View.OnClickListener {
 
@@ -12,11 +14,19 @@ public class InstructionsActivity extends Activity implements View.OnClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        final Button buttonGoBack=(Button)findViewById(R.id.buttongoback);
+
+        buttonGoBack.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v){
+
+        Intent i=new Intent(this,MenuActivity.class);
+        startActivity(i);
+        finish();
 
     }
 
